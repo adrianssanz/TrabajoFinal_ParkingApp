d9bb5dd51dc9e8eb32478500d049cecb6878a361
adriansanz <alumno.50291@ies-azarquiel.es>
2024-05-08 17:56:11 +0200

    Creado componente Ticket para tickets nuevos.

---
+++ parkingapp_front/src/app/components/ticket/ticket.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { DataService } from '../../services/data.service';
+
+@Component({
+  selector: 'app-ticket',
+  templateUrl: './ticket.component.html',
+  styleUrls: ['./ticket.component.css']
+})
+export class TicketComponent implements OnInit {
+  ticketData: any;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    this.ticketData = this.dataService.getTicketAñadido();
+    console.log('Ticket Data:', this.ticketData);
+  }
+}
