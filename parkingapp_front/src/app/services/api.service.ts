9ef0f12e343e0600b828efe02fe39421a4f40e0e
adriansanz <alumno.50291@ies-azarquiel.es>
2024-05-08 17:39:12 +0200

    Añadido metodo para añadir tickets a la api.

--- parkingapp_front/src/app/services/api.service.ts
+++ parkingapp_front/src/app/services/api.service.ts
@@ -1,15 +1,24 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-  apiUrl = 'http://localhost:8080/api/vehiculos';
+  apiUrl = 'http://localhost:8080/api';
 
   constructor(private http: HttpClient) { }
 
   getVehiculos() {
-    return this.http.get(this.apiUrl);
+    return this.http.get(`${this.apiUrl}/vehiculos`);
+  }
+
+  agregarVehiculo(vehiculo: any, tipoId: number): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/vehiculos/${tipoId}`, vehiculo);
+  }
+
+  agregarTicket(matricula: string): Observable<any> {
+    return this.http.post<any>(`${this.apiUrl}/tickets/${matricula}`, null);
   }
 }
