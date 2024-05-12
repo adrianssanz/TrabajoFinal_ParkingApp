d0b9d27b99873a1e9d40cdf10a762891cdc85f21
adrianssanz <sanzadrian@hotmail.es>
2024-05-12 13:11:23 +0200

    Añadida ruta componente lista-tickets

--- parkingapp_front/src/app/app-routing.module.ts
+++ parkingapp_front/src/app/app-routing.module.ts
@@ -5,8 +5,8 @@ import { ErrorComponent } from './error/error.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { AddVehiculoComponent } from './components/add-vehiculo/add-vehiculo.component';
 import { LoginComponent } from './components/login/login.component';
-import { TicketComponent } from './components/ticket/ticket.component';
 import { ListaTicketsComponent } from './components/lista-tickets/lista-tickets.component';
+import { TicketNuevoComponent } from './components/ticket-nuevo/ticket-nuevo.component';
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
@@ -14,7 +14,7 @@ const routes: Routes = [
   { path: 'lista-tickets', component: ListaTicketsComponent, canActivate:[AuthGuard]},
   { path: 'dashboard', component: DashboardComponent, canActivate:[AuthGuard],
   children: [
-    { path: "ticket", component: TicketComponent }
+    { path: "ticket", component: TicketNuevoComponent }
   ]},
   { path: '**', component: ErrorComponent }
   
