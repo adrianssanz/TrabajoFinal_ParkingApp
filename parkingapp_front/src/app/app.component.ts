4659f49aa5370aee785d25d341f57e1c020f3994
adrianssanz <sanzadrian@hotmail.es>
2024-05-15 21:25:24 +0200

    Añadido firebase.

--- parkingapp_front/src/app/app.component.ts
+++ parkingapp_front/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { claves } from '../claves';
+
+import firebase from 'firebase/compat/app';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'parkingapp_front';
+
+  ngOnInit(): void {
+    firebase.initializeApp({
+      apiKey: claves.apiKey,
+      authDomain: claves.authDomain
+    });
+  }
 }
