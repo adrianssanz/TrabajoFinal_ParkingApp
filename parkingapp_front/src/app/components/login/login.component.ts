cdf8bd20518c89fc21d19b25cd5e8f0521c1fc47
adrianssanz <sanzadrian@hotmail.es>
2024-05-15 21:25:06 +0200

    Componente login con inicio de sesión en firebase.

--- parkingapp_front/src/app/components/login/login.component.ts
+++ parkingapp_front/src/app/components/login/login.component.ts
@@ -1,37 +1,44 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from '../../services/api.service';
 import { Router } from '@angular/router';
-import { AuthService } from '../../services/auth.service';
+import { LoginService } from '../../services/login.service';
+import { DataService } from '../../services/data.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent implements OnInit {
-  vehiculos: any[] = [];
+  uidUsuario: string = '';
   matriculaSeleccionada: string = '';
+  email: string = '';
+  password: string = '';
+  mensajeError: string ='';
 
-  constructor(private apiService: ApiService, private authService: AuthService, private router: Router) { }
+  constructor(
+    private apiService: ApiService,
+    private dataService: DataService,
+    private router: Router,
+    private loginService: LoginService,
+  ) {}
 
   ngOnInit(): void {
-    this.getVehiculos();
+    
   }
 
-  getVehiculos(): void {
-    this.apiService.getVehiculos()
-  .subscribe((data: any) => {
-    this.vehiculos = data;
-  });
-
+  getMatricula(uid: string){
+    this.apiService.getVehiculoByUid(uid).subscribe((data:any)=>{
+      this.dataService.setSelectedMatricula(data.matricula);
+      console.log("Inicio de sesión exitoso.");
+      this.router.navigate(["/dashboard"]);
+    })
   }
 
   login(): void {
-    if (this.authService.login(this.matriculaSeleccionada)) {
-      this.router.navigate(['/dashboard']);
-    } else {
-      console.log('Inicio de sesión fallido');
-    }
+    this.loginService.login(this.email, this.password).then((response) => {
+      this.getMatricula(response.user?.uid || '');
+    }).catch(error=>(this.mensajeError = "Credenciales incorrectas.", console.log("ERROR: " + error)));
   }
 
   goToAddVehiculo(): void {
