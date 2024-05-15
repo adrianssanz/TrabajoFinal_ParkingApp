e5f2828e8a7069db6c51e3b34b5675f77a0b072a
adrianssanz <sanzadrian@hotmail.es>
2024-05-15 21:23:53 +0200

    Creado servicio de login con firebase.

---
+++ parkingapp_front/src/app/services/login.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LoginService {
+  constructor(private router: Router) {}
+
+  token: string = '';
+  uid: string='';
+
+  login(email: string, password: string) {
+    return firebase
+      .auth()
+      .signInWithEmailAndPassword(email, password);
+  }
+
+}
