a4ece9feba8ff179c9b97172a3935ae6366be783
adrianssanz <sanzadrian@hotmail.es>
2024-05-21 19:06:34 +0200

    Añadida duracion del ticket.

--- parkingapp_front/src/app/components/ticket/ticket.component.ts
+++ parkingapp_front/src/app/components/ticket/ticket.component.ts
@@ -8,4 +8,16 @@ import { Component, Input } from '@angular/core';
 export class TicketComponent {
   // Recoge los datos que mostrará en el componente ticket
   @Input() ticketData: any;
+
+  calcularDuracion(): number {
+    if (this.ticketData && this.ticketData.horaInicio && this.ticketData.horaFin) {
+      const inicio = new Date(this.ticketData.horaInicio);
+      const final = new Date(this.ticketData.horaFin);
+      const milisegundos = final.getTime() - inicio.getTime();
+      const horas = Math.floor(milisegundos / (1000 * 60 * 60));
+      return horas;
+    } else {
+      return 0;
+    }
+  }
 }
