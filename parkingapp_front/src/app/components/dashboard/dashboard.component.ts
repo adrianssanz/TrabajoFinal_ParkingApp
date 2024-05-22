1016c4d098da367f9891743515f750dc62635dd2
adrianssanz <sanzadrian@hotmail.es>
2024-05-22 19:07:21 +0200

    Añadida informacion extra del vehiculo y boton para el modal de iniciar ticket

--- parkingapp_front/src/app/components/dashboard/dashboard.component.ts
+++ parkingapp_front/src/app/components/dashboard/dashboard.component.ts
@@ -1,68 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { DataService } from '../../services/data.service';
 import { Router } from '@angular/router';
-import { ApiService } from '../../services/api.service';
 import { MatDialog } from '@angular/material/dialog';
 import { ModalComponent } from '../modal/modal.component';
+import { ModalAmpliarComponent } from '../modal-ampliar/modal-ampliar.component';
+import { Vehiculo } from '../../interfaces/vehiculo';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+  styleUrls: ['./dashboard.component.css'],
 })
 export class DashboardComponent implements OnInit {
-  selectedMatricula: String = '';
+  selectedMatricula: string = '';
   ticketData: any;
   mensaje: String = '';
+  vehiculo!: Vehiculo;
 
   constructor(
     private dataService: DataService,
     private router: Router,
-    private apiService: ApiService,
     private matDialog: MatDialog
-  ) { }
+  ) {}
 
   ngOnInit(): void {
+    //Recoge la matricula almacenada en el dataservice
     this.selectedMatricula = this.dataService.getSelectedMatricula();
+
+    //Alamcena el vehiculo del dataservice en la variable vehiculo para usarla despues
+    this.vehiculo = this.dataService.getVehiculo();
   }
 
-  abrirModal():void{
+  // Metodo para abrir el modal para añadir un ticket
+  abrirModal(): void {
     this.matDialog.open(ModalComponent);
   }
 
+  // Metodo para abrir el modal para ampliar un ticket
+  abrirModalAmpliar():void{
+    this.matDialog.open(ModalAmpliarComponent);
+  }
+
+  // Metodo para ir al login o "cerrar sesion"
   goToHome(): void {
     this.router.navigate(['/']);
     this.dataService.setSelectedMatricula('');
-    console.log("Sesión cerrada.");
+    console.log('Sesión cerrada.');
   }
 
+  // Metodo para ver la lista de tickets
   goToListaTickets(): void {
     this.router.navigate(['/lista-tickets']);
-  }
-
-  nuevoTicket(): void {
-    const horaInicioDate = new Date();
-    const ticket: { horaInicio?: string; horaFin?: string } = {};
-
-    const horaFinDate = new Date(horaInicioDate);
-    horaFinDate.setHours(horaFinDate.getHours() + 2);
-
-    const horaFin = horaFinDate.toISOString();
-
-    ticket.horaFin = horaFin;
-    ticket.horaInicio = horaInicioDate.toISOString();
-
-    this.apiService.agregarTicket(ticket, this.selectedMatricula).subscribe(
-      (response) => {
-        this.dataService.setTicketAñadido(response);
-        this.mensaje = 'Ticket añadido con éxito.';
-        console.log('Ticket agregado:', response);
-        this.router.navigate(['/dashboard/ticket']);
-      },
-      (error) => {
-        console.error('Error al agregar el ticket:', error.error);
-        this.router.navigate(['/dashboard/ticket']);
-      }
-    );
   }
 }
